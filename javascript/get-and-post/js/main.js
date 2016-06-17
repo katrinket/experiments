d716568d25144f9f0868b4a1bddd8858d79be4f7
katrna <katrina_dorn@yahoo.com>
2016-06-17 15:45:02 +0100

    @kdog some fun experiments

--- javascript/get-and-post/js/main.js
+++ javascript/get-and-post/js/main.js
@@ -19,3 +19,14 @@ function veryComplicatedMAthLibraryThatMightBeDoingInterestingThings(a,b){
 function kdogLog(stringToLog){
     console.log(new Date(),stringToLog);
 }
+
+
+function doAPost(){
+
+    //using jquery to do a get request for cart json (or some other feed or webservice)
+    $.post( "http://localhost:8001/server.php", {firstname: 'timmy', lastname:'hendrix'}, function(result){
+        console.log('resuly',result)
+        $("span").html(result);
+    });
+
+}
